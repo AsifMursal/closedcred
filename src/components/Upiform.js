735fdedd961364f4a1e5f53bcf342ffbd8e61cc0
Asif Mursal <asif.mursal21@vit.edu>
2023-08-25 21:14:17 +0530

    Added filr

---
+++ src/components/Upiform.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { useState,useEffect } from 'react';
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+import RPC from "../web3RPC";
+import { useNavigate } from 'react-router-dom';
+
+export default function Upiform() {
+
+ const [web3auth, setWeb3auth] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState("");
+  const [chainId, setChainId] = useState("");
+  const [userData, setUserData] = useState({});
+  const [UpiID, setUpiID] = useState("");
+    const navigate=useNavigate();
+
+    const clientId =
+    "BMkKHE4n2KgzLWFXDmpCVIpWMggQ8Pe8_4pRkbm9aNafKnn0WRlb1zoy6JlOh2nN2Aw54jIAbFbsAUut3tuJr8w";
+    useEffect(() => {
+      const init = async () => {
+        try {
+          const web3auth = new Web3Auth({
+            clientId,
+            chainConfig: {
+              chainNamespace: CHAIN_NAMESPACES.EIP155,
+              chainId: "0x13881",
+              rpcTarget: "https://rpc-mumbai.maticvigil.com/",
+            },
+  
+          });
+  
+          setWeb3auth(web3auth);
+          await web3auth.initModal();
+          setProvider(web3auth.provider);
+          console.log(provider);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      init();
+    }, []);
+
+      const sendata = async (UpiID) => {
+        const user = await web3auth.getUserInfo();
+        setUserData(user);
+        const Name=userData.name;
+
+        if (!provider) {
+          console.log("provider not initialized yet");
+          return;
+        }
+        console.log(provider)
+        const rpc = new RPC(provider);
+        const address = await rpc.getAccounts();
+        setAddress(address);
+        let AccountID=address;
+        console.log(address);
+        let result = await fetch("http://localhost:5000/login", {
+          method: "post",
+          body: JSON.stringify({ Name, AccountID, UpiID }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+        result = await result.json();
+        console.log(result);
+        alert("UPI ID submitted successfully");
+      };
+      const logout = async () => {
+        if (!web3auth) {
+          console.log("web3auth not initialized yet");
+          return;
+        }
+        const web3authProvider = await web3auth.logout();
+        setProvider(web3authProvider);
+        setBalance("");
+        setAddress("");
+        setUserData({});
+        setChainId("");
+        localStorage.removeItem('user');
+        navigate('/')
+      };
+
+      const getAccounts = async () => {
+        if (!provider) {
+          console.log("provider not initialized yet");
+          return;
+        }
+        console.log(provider)
+        const rpc = new RPC(provider);
+        const address = await rpc.getAccounts();
+        setAddress(address);
+        console.log(address);
+      };
+  return (
+    <div>
+      <input onChange={(e)=>{setUpiID(e.target.value);}} />
+      <button onClick={()=>{sendata(UpiID); }}> Submit</button>
+      <button onClick={logout}>LogOut</button>
+      
+    </div>
+  )
+}
